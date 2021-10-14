286c5edd5881fd278be5da650488f19231e77637
Tashfiq Chowdhury <tashfiqchowdhury@MacBook-Pro.local>
2021-10-14 02:59:13 -0700

    Star rating formatted

--- review-app/src/components/StarRating.js
+++ review-app/src/components/StarRating.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import { ReactComponent as Star} from './star.svg'
+import React from "react";
+import { ReactComponent as Star } from "./star.svg";
 
 const StarRating = (props) => {
-    const rating = props.rating;
-    // console.log(Array(rating));
-    // console.log([...Array(rating)])
-    return (
-        <div>
-            {[...Array(rating)].map((star, index) => (
-                <Star fill="#FDCC0D" key={index} />
-            ))}
-        </div>
-    )
-}
+  const rating = props.rating;
+  return (
+    <div>
+      {[...Array(rating)].map((star, index) => (
+        <Star fill="#FDCC0D" key={index} />
+      ))}
+    </div>
+  );
+};
 
 export default StarRating;
